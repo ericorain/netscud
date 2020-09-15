7e24916f9a5fec25860858e7507a5864141d3a21
Eric Orain <eric.orain@b-com.com>
2020-09-15 18:11:43 +0200

    Cisco IOS single password bug to fix

--- vendors/cisco/cisco_ios.py
+++ vendors/cisco/cisco_ios.py
@@ -5,5 +5,17 @@ class CiscoIOS(NetworkDevice):
     """
     Class for Cisco IOS devices
     """
-    
+    '''
+        
+
+    User Access Verification
+
+    Password:
+    % Password:  timeout expired!
+    Password:
+    SW3>en
+    Password:
+    Password:
+    SW3#
+    '''
     pass
