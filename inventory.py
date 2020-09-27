5639cbca941553ef6d407092a064dc624ea713de
ericorain <ericorain@hotmail.com>
2020-09-27 22:53:31 +0200

    Just a commit

--- inventory.py
+++ inventory.py
@@ -10,6 +10,7 @@ import yaml, logging
 =======
 logging.basicConfig(level=logging.INFO)
 <<<<<<< HEAD
+<<<<<<< HEAD
 >>>>>>> 0d922d3... Just a commit
 <<<<<<< HEAD
 =======
@@ -19,6 +20,8 @@ logging.basicConfig(level=logging.INFO)
 >>>>>>> ce6b77e... Just a commit
 =======
 >>>>>>> 7f1cda1... C:/Users/PC/AppData/Local/pypoetry/Cache/virtualenvs/netscud-QY00SrmA-py3.8/Scripts/python.exe d:/Prog/python/git/test.py
+=======
+>>>>>>> 402701c... Just a commit
 # logging.basicConfig(level=logging.WARNING)
 
 
@@ -96,6 +99,7 @@ class Inventory:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
             # Convert data (dict + list) into a dictionary (dict)
 =======
 >>>>>>> 0d922d3... Just a commit
@@ -109,6 +113,8 @@ class Inventory:
 >>>>>>> ce6b77e... Just a commit
 =======
 >>>>>>> 7f1cda1... C:/Users/PC/AppData/Local/pypoetry/Cache/virtualenvs/netscud-QY00SrmA-py3.8/Scripts/python.exe d:/Prog/python/git/test.py
+=======
+>>>>>>> 402701c... Just a commit
             device_dict = {**input_data[device], **{"name": device}}
 
             # Display info message
@@ -123,6 +129,7 @@ class Inventory:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
         # Return a list with the devices
 =======
 >>>>>>> 0d922d3... Just a commit
@@ -136,6 +143,8 @@ class Inventory:
 >>>>>>> ce6b77e... Just a commit
 =======
 >>>>>>> 7f1cda1... C:/Users/PC/AppData/Local/pypoetry/Cache/virtualenvs/netscud-QY00SrmA-py3.8/Scripts/python.exe d:/Prog/python/git/test.py
+=======
+>>>>>>> 402701c... Just a commit
         return list_of_devices
 
     def get_all_devices(self):
@@ -205,6 +214,7 @@ class Inventory:
 <<<<<<< HEAD
 <<<<<<< HEAD
 <<<<<<< HEAD
+<<<<<<< HEAD
 =======
 >>>>>>> 9bce50c... Select method for inventory and list of supported devices added
 =======
@@ -224,6 +234,8 @@ class Inventory:
 =======
 =======
 >>>>>>> 7f1cda1... C:/Users/PC/AppData/Local/pypoetry/Cache/virtualenvs/netscud-QY00SrmA-py3.8/Scripts/python.exe d:/Prog/python/git/test.py
+=======
+>>>>>>> 402701c... Just a commit
         return list_of_devices
 >>>>>>> 0d922d3... Just a commit
 =======
@@ -253,6 +265,7 @@ class Inventory:
                     # Yes
                     list_temp.append(device)
 
+<<<<<<< HEAD
 <<<<<<< HEAD
             # Save the temporary list into the list of devices
             list_of_devices = list_temp
@@ -289,6 +302,8 @@ class Inventory:
 >>>>>>> 0d922d3... Just a commit
 =======
 >>>>>>> 7f1cda1... C:/Users/PC/AppData/Local/pypoetry/Cache/virtualenvs/netscud-QY00SrmA-py3.8/Scripts/python.exe d:/Prog/python/git/test.py
+=======
+>>>>>>> 402701c... Just a commit
 # Main function call
 if __name__ == "__main__":
 
