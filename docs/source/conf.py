02e03ee78ff0a23736ab76722373949801858b9a
ericorain <ericorain@hotmail.com>
2020-10-12 21:19:16 +0200

    Another commit due to a problem with Read the docs...

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,6 +18,9 @@ sys.path.insert(0, os.path.abspath("../../.."))
 
 import sphinx_rtd_theme
 
+# The master toctree document.
+master_doc = 'index'
+
 # -- Project information -----------------------------------------------------
 
 project = "netscud"
