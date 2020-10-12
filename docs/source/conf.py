77aaae8cb983457cd83d42ff02ad9fe285b8c693
ericorain <ericorain@hotmail.com>
2020-10-12 21:29:45 +0200

    Remove master file from conf.py

--- docs/source/conf.py
+++ docs/source/conf.py
@@ -18,9 +18,6 @@ sys.path.insert(0, os.path.abspath("../../.."))
 
 import sphinx_rtd_theme
 
-# The master toctree document.
-master_doc = 'index'
-
 # -- Project information -----------------------------------------------------
 
 project = "netscud"
@@ -33,10 +30,7 @@ author = "Eric ORAIN"
 # Add any Sphinx extension module names here, as strings. They can be
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
-extensions = [
-    'sphinx_rtd_theme',
-    'sphinx.ext.autodoc'
-]
+extensions = ["sphinx_rtd_theme", "sphinx.ext.autodoc"]
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ["_templates"]
